c66f94f651e3d3321306bc05861a91d6a2a19540
KetutDana <ketutdana@KetutDanas-MacBook-Pro.local>
2025-03-30 12:27:10 +0800

    update auth folder

--- src/Resources/views/layouts/app.blade.php
+++ src/Resources/views/layouts/app.blade.php
@@ -1,10 +1,18 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'Xblade Auth')</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="flex items-center justify-center min-h-screen bg-gray-100">
     <div class="max-w-md w-full bg-white shadow-lg rounded-lg p-6">
--- src/XbladeAuthServiceProvider.php
+++ src/XbladeAuthServiceProvider.php
@@ -9,17 +9,24 @@ class XbladeAuthServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        // Memuat tampilan dari package ini
         $this->loadViewsFrom(__DIR__.'/Resources/views', 'xblade-auth');
 
-        // Publikasi file views agar bisa digunakan di aplikasi Laravel pengguna
         $this->publishes([
             __DIR__.'/Resources/views/auth' => resource_path('views/auth'),
             __DIR__.'/Resources/views/dashboard.blade.php' => resource_path('views/dashboard.blade.php'),
             __DIR__.'/Resources/views/layouts' => resource_path('views/layouts'),
+        
+            __DIR__.'/Resources/views/auth/login.blade.php' => resource_path('views/auth/login.blade.php'),
+            __DIR__.'/Resources/views/auth/register.blade.php' => resource_path('views/auth/register.blade.php'),
+            __DIR__.'/Resources/views/auth/forgot-password.blade.php' => resource_path('views/auth/forgot-password.blade.php'),
+            __DIR__.'/Resources/views/auth/reset-password.blade.php' => resource_path('views/auth/reset-password.blade.php'),
+            __DIR__.'/Resources/views/auth/verify-email.blade.php' => resource_path('views/auth/verify-email.blade.php'),
+            __DIR__.'/Resources/views/auth/confirm-password.blade.php' => resource_path('views/auth/confirm-password.blade.php'),
+            // __DIR__.'/Resources/views/auth/two-factor-challenge.blade.php' => resource_path('views/auth/two-factor-challenge.blade.php'),
+        
         ], 'xblade-auth-views');
+        
 
-        // Mendaftarkan command hanya jika berjalan di console
         if ($this->app->runningInConsole()) {
             $this->commands([
                 InstallCommand::class,
