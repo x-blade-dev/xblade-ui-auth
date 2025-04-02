459dcb90612facebe3eb3a0ca0beeb1035ec9987
KetutDana <ketutdana@KetutDanas-MacBook-Pro.local>
2025-04-02 13:28:20 +0800

    update service provider

--- src/XbladeAuthServiceProvider.php
+++ src/XbladeAuthServiceProvider.php
@@ -20,9 +20,21 @@ class XbladeAuthServiceProvider extends ServiceProvider
             __DIR__.'/Resources/views/auth' => resource_path('views/auth'),
             __DIR__.'/Resources/views/dashboard.blade.php' => resource_path('views/dashboard.blade.php'),
             __DIR__.'/Resources/views/layouts' => resource_path('views/layouts'),
-            __DIR__.'/Resources/views/profile' => resource_path('views/profile'),
-
+        
+            __DIR__.'/Resources/views/auth/login.blade.php' => resource_path('views/auth/login.blade.php'),
+            __DIR__.'/Resources/views/auth/register.blade.php' => resource_path('views/auth/register.blade.php'),
+            __DIR__.'/Resources/views/auth/forgot-password.blade.php' => resource_path('views/auth/forgot-password.blade.php'),
+            __DIR__.'/Resources/views/auth/reset-password.blade.php' => resource_path('views/auth/reset-password.blade.php'),
+            __DIR__.'/Resources/views/auth/verify-email.blade.php' => resource_path('views/auth/verify-email.blade.php'),
+            __DIR__.'/Resources/views/auth/confirm-password.blade.php' => resource_path('views/auth/confirm-password.blade.php'),
+        
         ], 'xblade-auth-views');
+
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                InstallCommand::class,
+            ]);
+        }
     }
 
     /**
